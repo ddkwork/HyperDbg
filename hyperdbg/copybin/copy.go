97a350dbfa500b5a97ffbbc1a8abc87b015789f3
Admin <2762713521@qq.com>
2025-07-17 09:59:20 +0800

    server

--- hyperdbg/copybin/copy.go
+++ hyperdbg/copybin/copy.go
@@ -21,12 +21,25 @@ func main() {
 			if strings.HasPrefix(filepath.Base(path), "CMake") {
 				return err
 			}
+			if strings.HasPrefix(filepath.Base(path), "Compiler") {
+				return err
+			}
 			bins = append(bins, path)
 		}
 		return err
 	})
 	mylog.Struct(bins)
+
+	targetDir := ""
+	filepath.Walk(cliDir, func(path string, info fs.FileInfo, err error) error {
+		if filepath.Base(path) == "debugger_server.exe" {
+			targetDir = filepath.Dir(path)
+			return err
+		}
+		return err
+	})
+
 	for _, bin := range bins {
-		stream.CopyFile(bin, filepath.Join(cliDir, filepath.Base(bin)))
+		stream.CopyFile(bin, filepath.Join(targetDir, filepath.Base(bin)))
 	}
 }
